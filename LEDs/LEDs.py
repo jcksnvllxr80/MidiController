c596c5ca5f2d1ecc657426d65d2e94f533a8dbef
jcksnvllxr80 <ac.watkins80@gmail.com>
2019-02-01 19:33:03 -0500

    fix naming collision

--- LEDs/LEDs.py
+++ LEDs/LEDs.py
@@ -3,6 +3,7 @@
 
 import time
 import Adafruit_GPIO as GPIO
+import RPi.GPIO as piGPIO
 import Adafruit_GPIO.I2C as I2C
 import MCP23017_R1
 
@@ -57,11 +58,11 @@ class Looper_LEDs(MCP23017_R1.MCP23017, object):
 	
 	def initPWM(self):
 		#set the mode for how the GPIO pins will be numbered
-		GPIO.setmode(GPIO.BCM)
+		piGPIO.setmode(piGPIO.BCM)
 		#set the list of pin numbers as outputs
-		GPIO.setup(self.PWM_PIN, GPIO.OUT)
+		piGPIO.setup(self.PWM_PIN, piGPIO.OUT)
 		#set freq and pin number to a PWM object
-		self._pwm = GPIO.PWM(self.PWM_PIN, self.FREQ).start(50)
+		self._pwm = piGPIO.PWM(self.PWM_PIN, self.FREQ).start(50)
 
 	def setAllPinsOutput(self):
 		for pin in self.allPins:
