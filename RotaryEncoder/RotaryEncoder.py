71c6516ec91baa815c98cb6f55680ab812ddf477
Aaron Watkins <ac.watkins80@gmail.com>
2019-02-12 00:28:32 -0500

    its late: typo

--- RotaryEncoder/RotaryEncoder.py
+++ RotaryEncoder/RotaryEncoder.py
@@ -137,7 +137,7 @@ class Rotary_Encoder(RgbKnob):
 			self.current_part = self.current_part.next
 
 
-		self.songs_menu = self.setup_menu.add_child("Songs", self.show_songs, self.load_song_func)
+		self.songs_menu = self.setup_menu.add_child("Songs", self.show_songs, self.show_songs)
 		self.parts_menu = self.setup_menu.add_child("Parts", self.show_parts_of_song)
 		self.bpm_menu = self.setup_menu.add_child("BPM", self.show_bpm_for_song)
 		self.pedal_menu = self.setup_menu.add_child("Pedals", self.show_pedal_states)
