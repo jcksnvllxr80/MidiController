2d88a53b4e68cd268312187200491a9047275059
Aaron Watkins <ac.watkins80@gmail.com>
2019-02-12 00:21:22 -0500

    reorder and rework menu change check

--- RotaryEncoder/RotaryEncoder.py
+++ RotaryEncoder/RotaryEncoder.py
@@ -177,10 +177,11 @@ class Rotary_Encoder(RgbKnob):
 			self.set_song_info_message()
 		elif self.menu.current_node.children:
 			self.set_children_message()
-		elif self.menu.current_node.menu_data_items:
-			self.set_menu_data_message()
 		elif self.menu.current_node.menu_data_func:
 			self.menu.current_node.menu_data_func()
+			self.set_menu_data_message()
+		elif self.menu.current_node.menu_data_items:
+			self.set_menu_data_message()
 		else:
 			print("Error!!")
 			self.set_message("Error!!")
