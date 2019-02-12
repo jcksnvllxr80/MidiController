e31e48e427ae9a1723314d29c3b40c11e10c9904
Aaron Watkins <ac.watkins80@gmail.com>
2019-02-12 01:22:02 -0500

    fix again 1

--- RotaryEncoder/RotaryEncoder.py
+++ RotaryEncoder/RotaryEncoder.py
@@ -178,10 +178,6 @@ class Rotary_Encoder(RgbKnob):
 			self.set_song_info_message()
 		elif self.menu.current_node.children:
 			self.set_children_message()
-		elif not self.menu.current_node.menu_data_func is None:
-			print(self.menu.current_node.menu_data_func)
-			self.menu.current_node.menu_data_func()
-			self.set_menu_data_message()
 		elif self.menu.current_node.menu_data_items:
 			self.set_menu_data_message()
 		else:
@@ -640,6 +636,8 @@ class RotaryPushButton(EffectLoops.ButtonOnPedalBoard, Rotary_Encoder):
 			
 			if deltaT < 0.5: #if the press was shorter than half a second
 				# select the item or go into the menu currently on the display
+				elif self.menu.current_node.menu_data_func:
+					self.menu.current_node.menu_data_func()
 				if self.menu.current_node.menu_data_items:
 					print("data_items")
 					self.menu.current_node.menu_data_dict[self.menu.current_node.menu_data_items[self.menu.current_node.menu_data_position]]()
