5c54c7c95f70c699af75ec24d6929c5a95a36678
Aaron Watkins <ac.watkins80@gmail.com>
2019-09-26 22:54:06 -0400

    reverse rotary encoder pb states

--- RotaryEncoder/RotaryEncoder.py
+++ RotaryEncoder/RotaryEncoder.py
@@ -622,17 +622,17 @@ class RotaryPushButton(EffectLoops.ButtonOnPedalBoard, Rotary_Encoder):
 	def switch_modes(self, mode=None):
 		if mode is None:
 			if self.is_engaged:
-				self.turn_on()
+				self.turn_off()
 				self.mode = "Pedal"
 			else:
-				self.turn_off()
+				self.turn_on()
 				self.mode = "Song"
 		else:
 			if mode == "Pedal":
-				self.turn_on()
+				self.turn_off()
 				self.mode = "Pedal"
 			else:
-				self.turn_off()
+				self.turn_on()
 				self.mode = "Song"
 		self.save_mode_to_default()
 			
