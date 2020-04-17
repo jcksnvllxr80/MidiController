f5d9a277e6960bdace7e3639c8eceaf84ea1cb36
Aaron Watkins <ac.watkins80@gmail.com>
2020-04-16 22:53:06 -0400

    logging on unload

--- RotaryEncoder/RotaryEncoder.py
+++ RotaryEncoder/RotaryEncoder.py
@@ -72,6 +72,7 @@ class RgbKnob(object):
 		self._green.stop()
 		self._blue.stop()
 		GPIO.cleanup()
+		logger.info("\nstopping PWM for the rotary pb.") 
 	
 	def set_brightness(self, v):
 		''' change the global brightness variable and apply to the current color
