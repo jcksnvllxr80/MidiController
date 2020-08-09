327020a6d62de076531f9e16b8058f6d66cb129b
Aaron Watkins <ac.watkins80@gmail.com>
2020-08-09 00:13:25 -0400

    another attempt

--- RotaryEncoder/RotaryEncoder.py
+++ RotaryEncoder/RotaryEncoder.py
@@ -356,7 +356,7 @@ class Rotary_Encoder(RgbKnob):
 
 
 	def show_midi_pedal_configuration_opts(self):
-		midi_pedal_name = self.midi_pedal_menu.children[self.menu.current_node.current_child].name
+		midi_pedal_name = self.midi_pedal_menu.children[self.midi_pedal_menu.current_child].name
 		self.midi_pedal_config_menu[midi_pedal_name].menu_data_items = []
 		self.midi_pedal_config_menu[midi_pedal_name].menu_data_prompt = self.midi_pedal_config_menu[midi_pedal_name].name + ":"
 		self.midi_pedal_menu.menu_data_position = 0
